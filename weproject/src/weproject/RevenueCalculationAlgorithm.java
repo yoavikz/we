9d1bed061d4779404ad21ee7c10cff40c495d58f
Yoav Zaltsman <yoav.zaltsman@gmail.com>
2019-06-26 18:41:20 +0300

    remove unnecessry print in algorithm

--- weproject/src/weproject/RevenueCalculationAlgorithm.java
+++ weproject/src/weproject/RevenueCalculationAlgorithm.java
@@ -4,18 +4,18 @@ import java.time.YearMonth;
 import utils.Consts;
 
 public class RevenueCalculationAlgorithm {
-	//calculate the revenue for a specific row in the data in a certain month
+	// calculate the revenue for a specific row in the data in a certain month
 	public int calculateRevenuePerMonth(String inputDate, String[] dataRow) {
 		return (int) (calculateRelativePartOfMonthForPayment(inputDate, dataRow)
 				* Integer.parseInt(dataRow[Consts.MONTHLY_PRICE_INDEX]));
 	}
 
-	//calculate the relative part of the month for payment calculation
+	// calculate the relative part of the month for payment calculation
 	private float calculateRelativePartOfMonthForPayment(String inputDate, String[] dataRow) {
 		int numOfDaysForPayment = AlgorithmUtil.calculateNumOfPayingDays(inputDate, dataRow);
-		System.out.println(numOfDaysForPayment);
 		String[] splittedInputDate = inputDate.split("-");
-		float relativePart = (float) numOfDaysForPayment / YearMonth.of(Integer.parseInt(splittedInputDate[0]), Integer.parseInt(splittedInputDate[1])).lengthOfMonth();
+		float relativePart = (float) numOfDaysForPayment / YearMonth
+				.of(Integer.parseInt(splittedInputDate[0]), Integer.parseInt(splittedInputDate[1])).lengthOfMonth();
 		return relativePart;
 	}
 }
